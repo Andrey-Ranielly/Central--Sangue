04f2441f04a8e4c772b62057914adfd0ffa22716
Andrey Ranielly <76213189+Andrey-Ranielly@users.noreply.github.com>
2024-06-08 20:33:09 -0300

    Update Register.js

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -12,10 +12,8 @@ function Register() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Verificar se todos os campos foram preenchidos
     if (email && cpf && password && confirmPassword && password === confirmPassword) {
       // Lógica de registro aqui
-      
       navigate('/home');
     } else {
       alert('Por favor, preencha todos os campos corretamente.');
@@ -24,27 +22,27 @@ function Register() {
 
   return (
     <div>
-      <AuthNavbar /> {/* Movendo o AuthNavbar para o topo */}
-      <div className="login-container"> {/* Adicionando a classe do arquivo CSS */}
-        <form onSubmit={handleSubmit} className="login-form"> {/* Adicionando a classe do arquivo CSS */}
+      <AuthNavbar />
+      <div className="login-container">
+        <form onSubmit={handleSubmit} className="login-form">
           <div>
             <label className='register-label'>Cadastro</label><br></br> <br></br>
-            <label className="email-label">Email:</label> {/* Adicionando a classe do arquivo CSS */}
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="email-input" /> {/* Adicionando a classe do arquivo CSS */}
+            <label className="email-label">Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="email-input" />
           </div>
           <div>
-            <label className="cpf-label">CPF:</label> {/* Adicionando uma nova classe do arquivo CSS */}
-            <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} className="cpf-input" /> {/* Adicionando uma nova classe do arquivo CSS */}
+            <label className="cpf-label">CPF:</label> 
+            <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} className="cpf-input" /> 
           </div>
           <div>
-            <label className="password-label">Senha:</label> {/* Adicionando a classe do arquivo CSS */}
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="password-input" /> {/* Adicionando a classe do arquivo CSS */}
+            <label className="password-label">Senha:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="password-input" />
           </div>
           <div>
-            <label className="confirm-password-label">Confirmar Senha:</label> {/* Adicionando uma nova classe do arquivo CSS */}
-            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="confirm-password-input" /> {/* Adicionando uma nova classe do arquivo CSS */}
+            <label className="confirm-password-label">Confirmar Senha:</label> 
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="confirm-password-input" /> 
           </div>
-          <button type="submit" disabled={!email || !cpf || !password || !confirmPassword || password !== confirmPassword} className="submit-button">Cadastrar</button> {/* Adicionando a classe do arquivo CSS */}
+          <button type="submit" disabled={!email || !cpf || !password || !confirmPassword || password !== confirmPassword} className="submit-button">Cadastrar</button>
         </form>
       </div>
     </div>
